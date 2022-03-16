e6659985705c09bc2576a0a42956e32723b2d982
animus77 <animus7@outlook.com>
2022-03-16 15:25:11 -0600

    validacion agregada

--- admin/crear.php
+++ admin/crear.php
@@ -5,23 +5,47 @@
 
     $db = conection();
 
+    $errores = [];
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        echo "<pre>";
-        var_dump($_POST);
-        echo "</pre>";
+        // extraccion de datos del metodo post
         $nombre = mysqli_real_escape_string($db, $_POST['nombre']);
         $apellido = mysqli_real_escape_string($db, $_POST['apellido']);
         $edad = mysqli_real_escape_string($db, $_POST['edad']);
 
-        $query = "INSERT INTO personas (nombre, apellido, edad) VALUES('$nombre', '$apellido', '$edad')";
+        // validacion de datos
+        if (!$nombre) {
+            $errores[] = 'Falta agregar el nombre';
+        }
+        if (!$apellido) {
+            $errores[] = 'Falta agregar el apellido';
+        }
+        if (!$edad) {
+            $errores[] = 'Falta agregar la edad';
+        }
 
-        $resultado = mysqli_query($db, $query);
+        // insercion de datos en la base de datos, si no se tienen errores
+        if (empty($errores)){
+            $query = "INSERT INTO personas (nombre, apellido, edad) VALUES('$nombre', '$apellido', '$edad')";
+            $resultado = mysqli_query($db, $query);
+        }
+
     }
 
     incluirTemplate('header');
 ?>
 
     <main>
+        <div>
+            <?php
+                foreach($errores as $error){
+                    echo "<ol>";
+                    echo "<li>$error</li>";
+                    echo "</ol>";
+
+                }
+            ?>
+        </div>
         <p>Pagina para la creacion de informacion</p>
         <form action="" method="POST">
             <div class="formulario">
